d9c8a0abc5b05e4951e13156288c9463effd871e
masezahedi <masezahedi@gmail.com>
2025-05-25 15:32:50 +0330

    commit

--- server/config/index.js
+++ server/config/index.js
@@ -1,11 +1,11 @@
 const API_ID = 24554364;
 const API_HASH = "5db6997246b3bc3b6a8ac6097b1ef937";
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3332;
 
 module.exports = {
   API_ID,
   API_HASH,
   JWT_SECRET,
-  PORT
+  PORT,
 };
