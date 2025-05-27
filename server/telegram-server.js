2bb2fc6c131bae9642240c372d89eb0a357cff31
masezahedi <masezahedi@gmail.com>
2025-05-27 17:06:20 +0330

    ssds

--- server/telegram-server.js
+++ server/telegram-server.js
@@ -601,7 +601,7 @@ initializeAllServices();
 // Update server listening configuration
 const isProduction = process.env.NODE_ENV === "production";
 const PORT = 3332;
-const HOST = isProduction ? "sna.freebotmoon.ir" : "localhost"; // Listen on all network interfaces
+const HOST = "sna.freebotmoon.ir"; // Listen on all network interfaces
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
